3f29498ae671d12b4d04f48b263498d9f258d400
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2023-05-20 21:05:36 -0500

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center">
+    <img width="240" src="./phpolar.svg" />
+</p>
+
+# PHPolar
+
+## A minimal PHP framework
+
+[![Coverage Status](https://coveralls.io/repos/github/phpolar/csrf-response-filter/badge.svg?branch=main)](https://coveralls.io/repos/github/phpolar/csrf-response-filter/badge.svg?branch=main) [![Latest Stable Version](http://poser.pugx.org/phpolar/csrf-response-filter/v)][def] [![Total Downloads](http://poser.pugx.org/phpolar/csrf-response-filter/downloads)][def] [![Latest Unstable Version](http://poser.pugx.org/phpolar/csrf-response-filter/v/unstable)][def] [![License](http://poser.pugx.org/phpolar/csrf-response-filter/license)][def] [![PHP Version Require](http://poser.pugx.org/phpolar/csrf-response-filter/require/php)][def]
+
+### Thresholds
+|Source Code Size|Memory Usage|
+|----------------|------------|
+|2.1 kB|70 kB|
+
+[def]: https://packagist.org/packages/phpolar/csrf-response-filter
---
+++ src/Http/Message/ResponseFilterPatternStrategy.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpolar\CsrfResponseFilter\Http\Message;
+
+use Phpolar\Http\Message\ResponseFilterStrategyInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Stringable;
+
+/**
+ * Attaches the request id to forms and links using a pattern match and replace algorithm
+ */
+final class ResponseFilterPatternStrategy implements ResponseFilterStrategyInterface
+{
+    public function __construct(
+        private Stringable $token,
+        private StreamFactoryInterface $streamFactory,
+        private string $requestId,
+    ) {
+    }
+
+    public function algorithm(
+        ResponseInterface $response,
+    ): ResponseInterface {
+        $contents = $response->getBody()->getContents();
+        $writeStream = $this->streamFactory->createStream();
+        $result = preg_replace(
+            [
+                "/<form(.*?)>(.*?)<\/form>/s",
+                "/<a href=(.*?)\?(.*?)>(.*?)<\/a>/s",
+            ],
+            [
+                sprintf(
+                    "<form$1>$2\n    <input type=\"hidden\" name=\"%s\" value=\"%s\" />\n</form>",
+                    $this->requestId,
+                    (string) $this->token,
+                ),
+                sprintf(
+                    "<a href=$1?%s=%s&$2>$3</a>",
+                    $this->requestId,
+                    urlencode(
+                        (string) $this->token,
+                    ),
+                )
+            ],
+            $contents,
+        );
+        $writeStream->write($result ?? "");
+        $writeStream->rewind();
+        return $response->withBody($writeStream);
+    }
+}
